073644c0a30e615d1af633a7b25e2e05b54c38b7
Nick Jones <nick.jones@meanbee.com>
2015-08-14 15:13:17 +0300

    added button in RapidCampaign system config to force fetch new promotions

---
+++ app/code/community/RapidCampaign/Promotions/Block/Promotional/Button.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * RapidCampaign Link Block
+ *
+ * @category    RapidCampaign
+ * @package     RapidCampaign_Promotions
+ * @copyright   Copyright (c) 2015 RapidCampaign (http://rapidcampaign.com)
+ */
+class RapidCampaign_Promotions_Block_Promotional_Button extends Mage_Adminhtml_Block_System_Config_Form_Field
+{
+    /**
+     * @param Varien_Data_Form_Element_Abstract $element
+     * @return string
+     */
+    protected function _getElementHtml(Varien_Data_Form_Element_Abstract $element)
+    {
+        $elementData = $element->getData();
+
+        $this->setElement($element);
+
+        $url = Mage::helper("adminhtml")->getUrl('rapidcampaign/adminhtml_promotion/update');
+
+        $html = $this->getLayout()->createBlock('adminhtml/widget_button')
+            ->setLabel($elementData['original_data']['button_label'])
+            ->setOnClick("setLocation('$url'); return false;")
+            ->toHtml();
+
+        return $html;
+    }
+}
---
+++ app/code/community/RapidCampaign/Promotions/controllers/Adminhtml/PromotionController.php
@@ -0,0 +1,47 @@
+<?php
+/**
+ * RapidCampaign
+ *
+ * @category    RapidCampaign
+ * @package     RapidCampaign_Promotions
+ * @copyright   Copyright (c) 2015 RapidCampaign (http://rapidcampaign.com)
+ */
+class RapidCampaign_Promotions_Adminhtml_PromotionController extends Mage_Adminhtml_Controller_Action
+{
+    const PROMOTIONS_FETCH_SUCCESS_MESSAGE = 'Promotions have been successfully updated.';
+    const PROMOTIONS_FETCH_FAIL_MESSAGE = 'Promotions have not been updated.';
+
+    /**
+     * Fetch and update promotions
+     */
+    public function updateAction()
+    {
+        /** @var RapidCampaign_Promotions_Model_Cache $promotionsCache */
+        $promotionsCache = Mage::getModel('rapidcampaign_promotions/cache');
+
+        /** @var Mage_Core_Model_Session $sessionModel */
+        $sessionModel = Mage::getSingleton('core/session');
+
+        try {
+            // Update cache
+            if ($promotionsCache->updateCache()) {
+                $sessionModel->addSuccess(
+                    $this->__(self::PROMOTIONS_FETCH_SUCCESS_MESSAGE)
+                );
+            } else {
+                $sessionModel->addWarning(
+                    $this->__(self::PROMOTIONS_FETCH_FAIL_MESSAGE)
+                );
+            }
+        } catch (Exception $e) {
+            $sessionModel->addError(
+                $this->__($e->getMessage())
+            );
+        }
+
+        // Redirect back to Rapidcampaign extension configuration
+        Mage::app()->getResponse()->setRedirect(
+            Mage::helper('adminhtml')->getUrl('adminhtml/system_config/edit/section/rapidcampaign_promotions')
+        );
+    }
+}
